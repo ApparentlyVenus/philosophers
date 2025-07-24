e998ef6bacc4c26a27b04662549567ae83655a4b
Omar Dana <odana@c1r3s3.42beirut.com>
2025-07-24 14:50:55 +0300

    updated release_forks logic, now releases depending on the order of taking forks (odd and even)

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 13:30:06 by odana             #+#    #+#             */
-/*   Updated: 2025/07/24 14:42:49 by odana            ###   ########.fr       */
+/*   Updated: 2025/07/24 14:50:35 by odana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,14 @@ int	take_forks(t_philo *philo)
 
 void	release_forks(t_philo *philo)
 {
-	pthread_mutex_unlock(philo->right_fork);
-	pthread_mutex_unlock(philo->left_fork);
+	if (philo->id % 2 == 1)
+	{
+		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
+	}
+	else
+	{
+		pthread_mutex_unlock(philo->left_fork);
+		pthread_mutex_unlock(philo->right_fork);
+	}
 }
